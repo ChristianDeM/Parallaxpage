67bb45f2bc6cdc8d28e0130d0847f9dbe8687e8e
ChristianDeM <Christian.d.martino@gmail.com>
2022-06-09 00:53:14 -0500

    update page esferas

--- js/main.js
+++ js/main.js
@@ -16,27 +16,51 @@ lax.addElements(".logo", {
         [1, 3],
       ],
       opacity: [
-        [0, 50],
+        [0, 90],
         [1, 0],
       ],
       translateY: [
         [0, 90],
-        [0, 100],
+        [0, 110],
       ],
     },
   });
 
   
-lax.addElements(".welocome", {
-  scrollY: {
+  lax.addElements(".esfera1", {
     scrollY: {
-      opacity: [
-        [500, 600],
-        [0, 1],
+       translateY: [
+         [90, 240],
+         [-90, -200],
+       ],
+      translateX: [
+        [90, 240],
+        [-650, 150],
+      ],
+      scale: [
+        [90, 240],
+        [-1, 1.1],
       ],
     },
-  },
-});
+  });
+
+  lax.addElements(".esfera2", {
+    scrollY: {
+      translateX: [
+        [180, 270],
+        [1650, 1050],
+      ],
+       translateY: [
+         [180, 270],
+         [-90, 200],
+       ],
+     
+      scale: [
+        [90, 240],
+        [-1, 1.1],
+      ],
+    },
+  });
 
 
 }
